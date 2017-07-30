1e75499cb19cf58b890ceb3c286f572118ed75bd
Dianne <dianneprinsescah@gmail.com>
2017-07-30 12:47:46 +0300

    The activity deletion bug

--- app/Activity.php
+++ app/Activity.php
@@ -25,3 +25,5 @@ class Activity extends Model
         });
     }
 }
+
+
--- resources/views/profiles/activities/created_reply.blade.php
+++ resources/views/profiles/activities/created_reply.blade.php
@@ -2,12 +2,12 @@
 
     @slot('heading')
         {{$profileUser->name}} replied to
-        <a href="{{$activity->subject->thread->path()}}">"{{$activity->subject->thread->title}}"</a>
+
+        <a href="{{ $activity->subject->thread->path() }}"> "{{ $activity->subject->thread->title }}" </a>
     @endslot
 
     @slot('body')
         {{ $activity->subject->body }}
     @endslot
+
 @endcomponent
-
-
