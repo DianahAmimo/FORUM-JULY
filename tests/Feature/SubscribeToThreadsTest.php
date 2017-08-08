85788ab49e80a8bbac4a643a2cecc221046f7a58
Dianne <dianneprinsescah@gmail.com>
2017-08-08 11:50:07 +0300

    Episode 43: Thread Subscriptions Part 4

--- tests/Feature/SubscribeToThreadsTest.php
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -18,12 +18,7 @@
 
            $this->post($thread->path() . '/subscriptions');
 
-           $thread->addReply([
-               'user_id' => auth()->id(),
-               'body' => 'Some reply here'
-           ]);
-
-//           $this->assertCount(1, auth()->user()->notifications);
+           $this->assertCount(1, $thread->fresh()->subscriptions);
         }
 
         /** @test */
