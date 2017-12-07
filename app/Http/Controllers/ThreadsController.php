e9858756e84d2129f15ccae4717e6fa1e559d3bb
Dianne <dianneprinsescah@gmail.com>
2017-12-07 05:56:13 +0300

    Episode 58: Don't forget to scan your files

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
 {
-    //ThreadsController constructor
+    //Create a new ThreadsController instance.
     public function __construct()
     {
         $this->middleware('auth')->except(['index', 'show']);
